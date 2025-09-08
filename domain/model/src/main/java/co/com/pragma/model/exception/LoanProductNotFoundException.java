c6596bd93cb9d6ab0a475d45e4ae4b65fffdd37a
Santiago <santiagovalenciarios@gmail.com>
2025-09-08 15:22:26 -0500

    feat(HU02-manejo de excepciones): Agrega excepciones personalizadas como 'LoanProductNotFoundException.java' y 'UserNotFoundException.java' que heredan de 'BusinessException.java'

--- domain/model/src/main/java/co/com/pragma/model/exception/LoanProductNotFoundException.java
+++ domain/model/src/main/java/co/com/pragma/model/exception/LoanProductNotFoundException.java
@@ -1,6 +1,6 @@
 package co.com.pragma.model.exception;
 
-public class LoanProductNotFoundException extends RuntimeException {
+public class LoanProductNotFoundException extends BusinessException {
     public LoanProductNotFoundException(String message) {
         super(message);
     }
--- domain/model/src/main/java/co/com/pragma/model/exception/UserNotFoundException.java
+++ domain/model/src/main/java/co/com/pragma/model/exception/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package co.com.pragma.model.exception;
 
-public class UserNotFoundException extends RuntimeException {
+public class UserNotFoundException extends BusinessException {
     public UserNotFoundException(String message) {
         super(message);
     }
