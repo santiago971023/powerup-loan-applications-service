bf57e03c76206816a87da92027a2cd0fba2b01d3
Santiago <santiagovalenciarios@gmail.com>
2025-09-14 17:57:45 -0500

    feat(HU04-paginación): Se agrega ruta en mi RouterRest.java que llama a mi metódo listApplications de mi handler

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/RouterRest.java
@@ -2,17 +2,22 @@ package co.com.pragma.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RouterRest {
     @Bean
     public RouterFunction<ServerResponse> loanApplicationRoutes(LoanApplicationHandler loanApplicationHandler) {
-        return route(POST("/api/v1/loan-app"), loanApplicationHandler::saveLoanApplication);
+        return route(
+                POST("/api/v1/loan-app").and(accept(MediaType.APPLICATION_JSON)),
+                loanApplicationHandler::saveLoanApplication
+        ).andRoute(GET("/api/v1/loan-app").and(accept(MediaType.APPLICATION_JSON)),
+                loanApplicationHandler::listApplications);
+
     }
 }
