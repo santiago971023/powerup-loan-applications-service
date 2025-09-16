41c6b49f0d79dbe8556071eef372f988d45811af
Santiago <santiagovalenciarios@gmail.com>
2025-09-16 16:25:41 -0500

    feat(HU04-paginación): Se settea salario a mi user retornado en findUserById, además se agregar logs para monitorear el porqué llega null el salario

--- infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/pragma/consumer/UserRepositoryAdapter.java
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/pragma/consumer/UserRepositoryAdapter.java
@@ -6,11 +6,13 @@ import co.com.pragma.model.user.gateways.UserRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 public class UserRepositoryAdapter implements UserRepository {
 
@@ -50,7 +52,11 @@ public class UserRepositoryAdapter implements UserRepository {
                     user.setName(dto.getName());
                     user.setLastname(dto.getLastname());
                     user.setEmail(dto.getEmail());
+                    user.setSalary(dto.getUserSalary());
+                    log.info("=== === === === === === === === === === === === ===");
+                    log.info(user.toString());
                     return user;
+
                 });
     }
 }
