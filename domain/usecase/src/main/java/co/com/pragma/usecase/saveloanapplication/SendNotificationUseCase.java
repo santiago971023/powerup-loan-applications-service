f4fabd645033ab2b06aab87cef1d0d1b70ebf5e1
Santiago <santiagovalenciarios@gmail.com>
2025-09-20 16:56:17 -0500

    feat(HU06-cambio-estado-solicitud): Se agrega clase de Caso de uso para enviar la notificación, está orquesta la lógica a nivel dominio para lograr la funcionalidad.

---
+++ domain/usecase/src/main/java/co/com/pragma/usecase/saveloanapplication/SendNotificationUseCase.java
@@ -0,0 +1,30 @@
+package co.com.pragma.usecase.saveloanapplication;
+
+
+import co.com.pragma.model.exception.NotificationEmailNotFoundException;
+import co.com.pragma.model.notification.Notification;
+import co.com.pragma.model.notification.gateways.NotificationGateway;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.logging.Logger;
+
+@RequiredArgsConstructor
+public class SendNotificationUseCase {
+
+    private final static Logger LOGGER = Logger.getLogger(SendNotificationUseCase.class.getName());
+
+    private final NotificationGateway notificationGateway;
+
+    public Mono<Void> sendStatusChangeNotification(Notification notification) {
+
+        LOGGER.info(" <== Iniciando caso de uso para enviar notificación de cambio de estado en una solicitud ==>");
+
+        if(notification.getUserEmail() == null || notification.getUserEmail().isBlank()) {
+            return Mono.error(new NotificationEmailNotFoundException("No hemos encontrado el email del destinatario."));
+        }
+
+        return notificationGateway.sendNotification(notification);
+    }
+
+}
